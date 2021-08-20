03f3039881bc5a722bc888a03479f43be81d835c
sne-syn <snezhana.syngur@gmail.com>
2021-08-20 09:40:51 +0200

    Take values from form input

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdown-form');
 const dateElement = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateElement.setAttribute('min', today);
+
+// Take Values from Form Input
+const updateCountdown = (evt) => {
+  evt.preventDefault();
+  countdownTitle = evt.srcElement[0].value;
+  countdownDate = evt.srcElement[1].value;
+}
+
+// Event Listener
+countdownForm.addEventListener('submit', updateCountdown);
